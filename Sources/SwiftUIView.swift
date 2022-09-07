7fc7b6714e20558b56ff622753484836a7ca1f40
Mykhaylo Levchuk <m.levchuk@dook.pro>
2022-09-07 10:32:32 +0200

    initial commit

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Mykhaylo Levchuk on 07/09/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+@available(iOS 13.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
