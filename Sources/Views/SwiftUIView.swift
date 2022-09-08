7afb959d16675d24ef75dfce8a98dee0a933737e
Mykhaylo Levchuk <m.levchuk@dook.pro>
2022-09-08 11:20:40 +0200

    add image

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -14,6 +14,9 @@ public struct SwiftUIView: View {
     public init () {}
     public var body: some View {
         Text("This is the view from the package")
+        Image(uiImage: UIImage(named: "map", in: .module , with: nil)!)
+            .resizable().frame(width: 100, height: 100, alignment: .center)
+            .aspectRatio(contentMode: .fit)
     }
 }
 
